73f05cf9d7e39e872ab39dc3a69d5b7cd3716655
Abhishek Choudhary <a.choudhary@ieee.org>
2020-12-31 02:45:24 +0000

    Updated Fedora preinstall for FC33

--- preinstall_fedora.sh
+++ preinstall_fedora.sh
@@ -1,10 +1,6 @@
 #!/bin/bash -f
 
 #Preinstall reqs for Fedora
-#for n in konsole
-for n in kommander gawk xpdf mailx sendmail flex bison gcc gcc-c++ gcc-java libX11-devel kdebase
-	do 
-		yum install $n
-	done
+tum install -y gawk xpdf mailx sendmail flex bison gcc gcc-c++ gcc-java libX11-devel
 ln /usr/bin/bison /usr/bin/yacc
 
