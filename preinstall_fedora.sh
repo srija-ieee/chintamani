83c4e4a7fd049d5364fcbc29123e9f0dbf019434
Abhishek Choudhary <a.choudhary@ieee.org>
2020-12-31 03:53:37 +0000

    More fixes for Hindawi 2021

--- preinstall_fedora.sh
+++ preinstall_fedora.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -f
 
 #Preinstall reqs for Fedora
-tum install -y gawk xpdf mailx sendmail flex bison gcc gcc-c++ gcc-java libX11-devel
-ln /usr/bin/bison /usr/bin/yacc
+yum install -y konsole xpdf \
+    mailx sendmail \
+    flex flex-devel bison bison-devel gawk \
+    gcc gcc-c++ \
+    java-1.8.0-openjdk-devel \
+    python3 \
+    libX11-devel
+ln -sf /usr/bin/bison /usr/bin/yacc
 
