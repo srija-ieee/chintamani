f60ef28d2e177426df116775c7eb80f4bd0534ab
Abhishek Choudhary <a.choudhary@ieee.org>
2021-09-08 16:50:18 +0530

    First draft of Telugu Python

---
+++ Hindawi/soochee/keys.py
@@ -0,0 +1,38 @@
+and      మరియు
+as       వంటి
+assert   చాటుకోవడం
+async    అసమకాలీక
+await    ఎదురు
+break    విడతీయు
+class    తరగతి
+continue కొనసాగించు
+def	 నిర్వచించు
+del      తొలగించు
+elif     వేరే_ఉంటే
+else     వేరే
+except   తప్ప
+exec     అమలుపరచు
+false    తప్పు
+finally  చివరకు
+for      కోసం
+from     నుండి
+global   ప్రపంచ
+if       ఉంటే
+import   దిగుమతి
+in       లో
+is       ఉంది
+lambda	
+None     ఏమీ కాదు
+nonlocal స్థానికం _కానిది
+not      కాదు
+or       లేదా
+pass     ఉతీర్ణత
+print    ముద్రించు
+raise    పెంచు
+return   తిరిగి
+true     నిజం
+try      ప్రయత్నించు
+while    అయిత
+with     తో
+yield    దిగుబడి
+
